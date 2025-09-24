65b688d5b9b76733a738274679aae90484590cf8
Albreves73 <albreves@hotmail.com>
2025-09-23 21:13:16 -0400

    Update simulate.js

--- simulate.js
+++ simulate.js
@@ -1,7 +1,5 @@
-// Simula dados de sessão para testes locais
 sessionStorage.setItem("user_name", "Ana");
-sessionStorage.setItem("plan", "starter"); // pode ser: free, starter, pro, enterprise
+sessionStorage.setItem("plan", "starter");
 sessionStorage.setItem("access_token", "demo-token-123");
-
-// Redireciona para o painel
 window.location.href = "dashboard.html";
+
