f9eaa6741c2b26add37c38d2240e8a3f8840157f
Albreves73 <albreves@hotmail.com>
2026-07-29 18:47:22 -0400

    Initialize Express server with payment routes

---
+++ monkybite-backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+// Rotas
+const paymentRoutes = require('./routes/paymentRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Rotas da API
+app.use('/api', paymentRoutes);
+
+// Porta do servidor
+app.listen(3000, () => {
+  console.log('Backend running on port 3000');
+});
