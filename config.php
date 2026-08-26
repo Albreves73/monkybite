4e6e773ccdcbdb9bb8ff3d010b88fd7486fe285c
Albreves73 <albreves@hotmail.com>
2026-08-26 11:11:39 -0400

    Update config.php

--- config.php
+++ config.php
@@ -8,9 +8,15 @@ return [
 
     "nextcloud_host" => "https://cloud.monkybite.com",
 
-    // 🔥 Coloque sua senha REAL aqui depois
     "admin_user" => "admin",
     "admin_pass" => "Cu214200@@$",
+
+    // ============================================
+    // SQUARE CONFIGURATION
+    // ============================================
+
+    "square_access_token" => "EAAAlz_CU24QwkuDeXtJQQ6zg1qRviQZ2ESc7kLDmm1hHP3hPCOrC9qEp2TL4pYw",
+    "square_location_id" => "LTZ1WY5B11Q9Q",
 
     // ============================================
     // PLANOS, QUOTAS E GRUPOS
